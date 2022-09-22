c2bed9ad12b6caa16e3146b64335f43bc6479c09
pedrosait <pedro.desarrollo.sait@gmail.com>
2022-09-22 10:48:18 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Andromeda
+
 Vue practice
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import '../assets/Navbar.css'
+</script>
+
+<template>
+    <div class="container">
+        <nav>
+            <h2 class="logo">Andro<span>meda</span></h2>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+            <button type="button">Subscribe</button>
+        </nav>
+    </div>
+</template>
